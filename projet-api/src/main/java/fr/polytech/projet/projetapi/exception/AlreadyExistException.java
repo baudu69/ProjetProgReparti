e00a8575417f4555ff85cf757beb843f562df7d5
baudu <bastien.audu@gmail.com>
2022-06-08 14:33:52 +0200

    base

--- projet-api/src/main/java/fr/polytech/projet/projetapi/exception/AlreadyExistException.java
+++ projet-api/src/main/java/fr/polytech/projet/projetapi/exception/AlreadyExistException.java
@@ -1,4 +1,8 @@
 package fr.polytech.projet.projetapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class AlreadyExistException extends RuntimeException{
 }
--- projet-ui/src/app/app.module.ts
+++ projet-ui/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { ListeApprenantComponent } from './apprenant/liste-apprenant/liste-apprenant.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AccueilComponent,
+    ListeApprenantComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
