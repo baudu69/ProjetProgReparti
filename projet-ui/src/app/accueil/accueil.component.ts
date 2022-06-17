9ea3147bd8d9f355e1874669133d39a44cedca62
brsantoine <antoinebre@orange.fr>
2022-06-17 16:49:38 +0200

    Page d'accueil

--- projet-ui/src/app/accueil/accueil.component.ts
+++ projet-ui/src/app/accueil/accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-accueil',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  currentDate = new Date();
+
+  constructor() {}
 
   ngOnInit(): void {
   }
